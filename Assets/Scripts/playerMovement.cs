5fb678b7bd3bcfe1d564abadd674c67a49c8fe27
thg0brt <thiago.13.brito@gmail.com>
2019-08-31 16:52:58 -0300

    Recovered

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -5,13 +5,33 @@ public class playerMovement : MonoBehaviour {
 
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-
+    Animator animator;
     Vector2 movement;
+
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
 
 	// Update is called once per frame
 	void Update () {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if(movement.x == 1){
+            animator.SetInteger("direction", 2);
+        }else
+        if(movement.x == -1){
+            animator.SetInteger("direction", 3);
+        }else
+        if(movement.y == 1){
+            animator.SetInteger("direction", 1);
+        }else
+        if(movement.y == -1){
+            animator.SetInteger("direction", 0);
+        }/*else {
+            animator.SetInteger("direction", 4);
+        }*/
+
 	}
 
     void FixedUpdate(){
