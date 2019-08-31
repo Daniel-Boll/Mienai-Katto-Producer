07e0f7945215d4e18b74d0f5a4a6693ea258951f
Marcio Aguiar <marcionix.master@gmail.com>
2019-08-31 18:30:57 -0300

    eventos dos botoes
    aaaaa

---
+++ Assets/Scripts/ButtonEvents.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonEvents : MonoBehaviour
+{
+    public void StartEvent() {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitEvent() {
+        Application.Quit();
+    }
+}
