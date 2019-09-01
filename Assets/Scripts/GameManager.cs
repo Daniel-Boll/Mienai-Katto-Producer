f9f02439441e3a80257135868bf412373a8c6069
Marcio Aguiar <marcionix.master@gmail.com>
2019-08-31 21:19:31 -0300

    Elementos adicionais
    Elementos adicionais de cenário e inserção do gamemanager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int lastCorridor = 1;
+
+    void Awake() {
+        int n = GameObject.FindObjectsOfType<GameManager>().Length;
+        if (n == 1) {
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void SetLastCorridor(int index) {
+        lastCorridor = index;
+    }
+
+    public int GetLastCorridor() {
+        return lastCorridor;
+    }
+
+}
